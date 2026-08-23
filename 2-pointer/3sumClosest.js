faa71b074e0685dc56e15cba336294cae928681e
sahil kumar <sahilkumar06804@gmail.com>
2026-08-23 23:42:00 +0530

    2-pointer

---
+++ 2-pointer/3sumClosest.js
@@ -0,0 +1,35 @@
+var threeSumClosest = function(nums, target) {
+   
+    nums.sort((a, b) => a - b);
+   
+    let closestSum = nums[0] + nums[1] + nums[2];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+       
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            let currentSum = nums[i] + nums[left] + nums[right];
+
+            if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                closestSum = currentSum;
+            }
+
+      
+            if (currentSum === target) {
+                return currentSum;
+            } 
+            else if (currentSum < target) {
+                left++; 
+            } 
+            else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
+};
