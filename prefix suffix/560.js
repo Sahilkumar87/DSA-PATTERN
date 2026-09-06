fc4b6dc4e619513031e493cc0b677e701dd21194
sahil kumar <sahilkumar06804@gmail.com>
2026-09-06 22:52:42 +0530

    subarr sum equal k

---
+++ prefix suffix/560.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let sum = 0;
+    let res = 0;
+    let map = new Map();
+    map.set(0,1);
+
+    for(let i = 0; i<nums.length; i++){
+        sum += nums[i];
+        let qes = sum - k;
+        let freq = map.get(qes) || 0;
+        res += freq;
+        map.set(sum, (map.get(sum) || 0) + 1);
+    }
+    return res;
+    
+};
