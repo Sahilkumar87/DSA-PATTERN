7d8e9d6f92e65a37990862e9eb89e18e47c70d87
sahil kumar <sahilkumar06804@gmail.com>
2026-09-20 23:36:02 +0530

    gfg problem stack

---
+++ stack-pattern/gfg-nextSmaller.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @returns {number[]}
+ */
+class Solution {
+    nextSmallerEle(arr) {
+        // code here
+        let n = arr.length;
+                       let res = new Array(n);
+                       let st = [];
+
+                       res[n - 1] = -1;
+                       st.push(arr[n - 1]);
+
+                       for (let i = n - 2; i >= 0; i--) {
+
+                           while (st.length !== 0 && st[st.length - 1] >= arr[i]) {
+                               st.pop();
+                           }
+
+                           if (st.length === 0) {
+                               res[i] = -1;
+                           } else {
+                               res[i] = st[st.length - 1];
+                           }
+
+                           st.push(arr[i]);
+                       }
+
+                       return res;
+    }
+}
