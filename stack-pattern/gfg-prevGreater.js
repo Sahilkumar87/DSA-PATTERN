656675fd9eb889e9e6913e29ab9811947445439e
sahil kumar <sahilkumar06804@gmail.com>
2026-09-20 23:44:38 +0530

    solving problem in gfg

---
+++ stack-pattern/gfg-prevGreater.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} arr
+ * @returns {number[]}
+ */
+class Solution {
+    preGreaterEle(arr) {
+        // code here
+                    let n = arr.length;
+                    let res = new Array(n);
+                    let st = [];
+
+                    res[0] = -1;
+                    st.push(arr[0]);
+
+                    for (let i = 1; i<=n-1; i++) {
+
+                        while (st.length != 0 && st[st.length - 1] <= arr[i]) {
+                            st.pop();
+                        }
+
+                        if (st.length === 0) {
+                            res[i] = -1;
+                        } else {
+                            res[i] = st[st.length - 1];
+                        }
+
+                        st.push(arr[i]);
+                    }
+
+                    return res;
+        
+    }
+}
